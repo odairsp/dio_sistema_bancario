a17bf5a8d15bf891dd6337e074be467c6fb336ee
Odair H <odair.sp@hotmail.com>
2024-07-03 15:27:56 -0300

    Changes to be committed: 	modified:   sistema_bancario_v1.py

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -17,31 +17,46 @@ while True:
     opcao = input(menu)
     
     if opcao == "d":
-        print("DEPOSITAR\n\n")
+        print("DEPOSITAR".center(20,"-")+"\n\n")
         
         deposito = float(input("Digite o valor para depósito: =>"))
         
-        if deposito <= 0:
-            print("ERRO - Valor inválido! Repita a operação!")
-        else:
+        if deposito > 0:
             saldo += deposito
-            extrato += f"Depósito    R$ {deposito:.2f}\n"
-        
-    elif opcao == "s":
-        print("SACAR\n\n")
-        
-        saque = float(input("Digite o valor de saque: =>"))
-        
-        if saque <= 0 or saque > saldo or numero_saques >= LIMITE_SAQUES :
-            print("ERRO - Valor inválido! Repita a operação!")
+            extrato += "Depósito".ljust(10)+f"R$ {deposito:.2f}\n"
         else:
-            saldo -= saque
-            extrato += f"Saque    R$ {deposito:.2f}\n"
+            print("ERRO - Valor inválido! Repita a operação!")
+            
+    elif opcao == "s":
+        print("SACAR".center(20,"-")+"\n\n")
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saques:
+           print(f"ERRO - Excedeu quantidade de saques diários!")
         
+        else:
+            saque = float(input("Digite o valor de saque: =>"))
+            excedeu_saldo = saque > saldo
+            excedeu_limite = saque > limite
+            
+            
+            if excedeu_saldo:
+                print(f"ERRO - Saldo = R$ {saldo:.2f} insuficiente! Digite outro valor!")
+            elif saque <= 0:
+                print("ERRO - Valor inválido! Repita a operação!")
+            elif saque > limite:
+                print(f"ERRO - Valor acima do limite de R$ {limite:.2f}.! Digite outro valor!")
+            else:
+                saldo -= saque
+                numero_saques += 1
+                extrato += "Saque".ljust(10) + "R$ "+ f"saque:.2f \n"
+                print(f"Saque de R$ {saque:.2f} realizado com sucesso!")
+                print(f"Saldo atual é de R$ {saldo:.2f}.")
     elif opcao == "e":
-        print("EXTRATO\n\n")
-        print(extrato)
+        print("EXTRATO".center(20,"-")+"\n\n")
         
+        print("Nenhuma operação realizada!"if extrato=="" else extrato)
+        print(f"Saldo: R$ {saldo:.2f}.")
     elif opcao == "q":
         print("SAIR\n\n")
         break
